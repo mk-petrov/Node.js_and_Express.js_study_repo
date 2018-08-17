60310abd0973227acad327c1bf982d798dc951e7
mk-petrov <petko.petrov89@gmail.com>
2018-08-17 14:07:40 +0200

    added image upload

--- StopShop/handlers/home.js
+++ StopShop/handlers/home.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const url = require('url')
 const path = require('path')
+const database = require('../config/database')
+const qs = require('querystring')
 
 module.exports = (req, res) => {
   req.pathname = req.pathname || url.parse(req.url).pathname
@@ -20,7 +22,28 @@ module.exports = (req, res) => {
       res.writeHead(200, {
         'Content-Type': 'text/html'
       })
-      res.write(data)
+
+      let queryData = qs.parse(url.parse(req.url).query)
+
+      let products = database.products.getAll()
+
+      if (queryData.query) {
+        products = products.filter((e) => e.name.toLowerCase().indexOf(queryData.query.toLowerCase()) > -1)
+        // or
+        // products = products.filter((el) => el.name.toLowerCase().includes(queryData.query.toLowerCase()))
+      }
+      let content = ''
+      for (let product of products) {
+        content +=
+          `<div class="product-card">
+            <img class="product-img" src="${product.image}">
+            <h2>${product.name}</h2>
+            <p>${product.description}</p>
+          </div>`
+      }
+
+      let html = data.toString().replace('{content}', content)
+      res.write(html)
       res.end()
     })
   } else {
--- StopShop/handlers/product.js
+++ StopShop/handlers/product.js
@@ -3,6 +3,8 @@ const path = require('path')
 const database = require('../config/database')
 const url = require('url')
 const qs = require('querystring')
+const multiparty = require('multiparty')
+const shortid = require('shortid')
 
 module.exports = (req, res) => {
   req.pathname = req.pathname || url.parse(req.ulr).pathname
@@ -22,16 +24,55 @@ module.exports = (req, res) => {
       res.end()
     })
   } else if (req.pathname === '/product/add' && req.method === 'POST') {
-    let dataString = ''
+    let form = new multiparty.Form()
+    let product = ''
 
-    req.on('data', (data) => {
-      dataString += data
+    form.on('part', (part) => {
+      if (part.filename) {
+        let fileExtension = part.filename.split('.')[1]
+        let dataString = ''
+
+        part.setEncoding('binary')
+        part.on('data', (data) => {
+          dataString += data
+        })
+        product = qs.parse(dataString)
+
+        part.on('end', () => {
+          let fileName = shortid.generate()
+          let filePath = `/content/images/${fileName + '.' + fileExtension}`
+
+          product.image = filePath
+
+          fs.writeFile(`.${filePath}`, dataString, {encoding: 'ascii'}, (err) => {
+            if (err) {
+              console.log(err)
+            }
+          })
+        })
+      } else {
+        part.setEncoding('utf8')
+        let field = ''
+        part.on('data', (data) => {
+          field += data
+        })
+
+        part.on('end', () => {
+          product[part.name] = field
+        })
+      }
+    })
+    form.on('close', () => {
+      database.products.add(product)
+      res.writeHead(302, {
+        Location: '/'
+      })
+      res.end()
     })
 
-    req.on('end', () => {
-      let product = qs.parse(dataString)
-    })
+    form.parse(req)
   } else {
-    // to do
+    // Process as not handled request
+    return true
   }
 }
