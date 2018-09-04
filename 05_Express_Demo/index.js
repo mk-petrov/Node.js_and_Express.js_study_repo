a3c150da258f59c290ec5f78e5bd2dc0cf6caebc
mk-petrov <petko.petrov89@gmail.com>
2018-09-04 10:35:26 +0200

    serve static added

--- 05_Express_Demo/index.js
+++ 05_Express_Demo/index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 2557
+const path = require('path')
+
+app.use(express.static('public'))
+// app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express!')
+  res.sendFile(path.join(__dirname, '/public/html/index.html'))
+})
+
+app.post('/', (req, res) => {
+  res.send('Hi')
+  console.log(req.body)
 })
 
 app.get('/users/:userId/books/:bookId', (req, res) => {
